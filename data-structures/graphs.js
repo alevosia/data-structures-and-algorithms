7f27f8832b2504ce4a6a53c953f66caa59733cd0
Alexander Paul <alexanderpaul.marinas@gmail.com>
2020-07-15 18:05:18 +0800

    Traversing graphs

--- data-structures/graphs.js
+++ data-structures/graphs.js
@@ -39,6 +39,90 @@ class Graph {
 
 		console.log(this.aList)
 	}
+
+	// DFSRecursive(vertex) {
+	// 	console.log(vertex)
+
+	// 	for (const key in this.aList) {
+	// 		this.aList[key] = this.aList[key].filter((v) => v !== vertex)
+	// 	}
+
+	// 	while (this.aList[vertex][0]) {
+	// 		this.DFSRecursive(this.aList[vertex][0])
+	// 	}
+	// }
+
+	DFSRecursive(start) {
+		const result = []
+		const visited = {}
+		const aList = this.aList
+
+		function dfs(vertex) {
+			if (!vertex) return
+
+			visited[vertex] = true
+			result.push(vertex)
+
+			aList[vertex].forEach((neighbor) => {
+				if (!visited[neighbor]) dfs(neighbor)
+			})
+		}
+
+		dfs(start)
+
+		return result
+	}
+
+	DFSIterative(start) {
+		const result = []
+		const stack = []
+		const visited = {}
+
+		stack.push(start)
+
+		while (stack.length > 0) {
+			const vertex = stack.pop()
+
+			if (!visited[vertex]) {
+				result.push(vertex)
+				visited[vertex] = true
+
+				this.aList[vertex].forEach((neighbor) => {
+					stack.push(neighbor)
+				})
+			}
+		}
+
+		console.log(result)
+
+		return result
+	}
+
+	BFS(start) {
+		const queue = []
+		const result = []
+		const visited = {}
+
+		queue.push(start)
+		visited[start] = true
+
+		while (queue.length > 0) {
+			const vertex = queue.shift()
+
+			result.push(vertex)
+
+			this.aList[vertex].forEach((neighbor) => {
+				if (!visited[neighbor]) {
+					visited[neighbor] = true
+					queue.push(neighbor)
+				}
+			})
+		}
+
+		console.log(result)
+
+		return result
+	}
 }
 
 const graph = new Graph()
@@ -51,12 +135,12 @@ graph.addVertex('E')
 graph.addVertex('F')
 
 graph.addEdge('A', 'B')
-graph.addEdge('B', 'C')
-graph.addEdge('C', 'D')
+graph.addEdge('A', 'C')
+graph.addEdge('B', 'D')
+graph.addEdge('C', 'E')
 graph.addEdge('D', 'E')
+graph.addEdge('D', 'F')
 graph.addEdge('E', 'F')
-graph.addEdge('F', 'A')
 console.log(graph)
 
-graph.removeVertex('A')
-graph.removeVertex('B')
+graph.BFS('A')
